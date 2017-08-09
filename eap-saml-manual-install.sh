a7295d71c09a691b7975290eb7570151601b85b0
Miguel Delgadillo Hernandez <mdelgadi@redhat.com>
2017-08-09 02:03:18 -0500

    Modifying secure-saml-deployments

--- eap-saml-manual-install.sh
+++ eap-saml-manual-install.sh
@@ -2,8 +2,8 @@
 
 oc process eap70-https-s2i \
   APPLICATION_NAME=profile \
-  HOSTNAME_HTTPS=profile-secure.dempsey-training2.apps.latest.xpaas \
-  SOURCE_REPOSITORY_URL=https://github.com/maschmid/sso-demo.git \
+  HOSTNAME_HTTPS=profile-secure.sso-training-demo.192.168.42.198.nip.io \
+  SOURCE_REPOSITORY_URL=https://github.com/mikeintoch/sso-demo.git \
   SOURCE_REPOSITORY_REF=master \
   CONTEXT_DIR=app-profile-jee-saml \
   HTTPS_SECRET=eap-profile-secret \
@@ -15,5 +15,4 @@ oc process eap70-https-s2i \
   JGROUPS_ENCRYPT_NAME=jgroups-key \
   JGROUPS_ENCRYPT_PASSWORD=password \
   | oc create -f - 
-
 
