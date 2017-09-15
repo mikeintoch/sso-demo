f3cbb29d26903b2c49198c4f62b17232d2be9c0e
Miguel Delgadillo Hernandez <mdelgadi@redhat.com>
2017-09-14 19:48:12 -0500

    Deleting sh files and adding yaml files

--- prepare-image-streams-and-templates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-oc create -f sso70-image-stream.json
-oc create -f sso70-postgresql-persistent.json
-
-oc create -f eap70-image-stream.json
-oc create -f eap70-sso-s2i.json
-oc create -f eap70-https-s2i.json
